d7c0ded7aba9b5e7b77b4ce2fa1f9163f9bf677a
TranquilityStudio <egoriesgames.gamestudio@gmail.com>
2023-09-24 16:50:22 +0500

    Uploading the necessary files to the project.

---
+++ JoinBroadcast/EventHandler.cs
@@ -0,0 +1,22 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoinBroadcast
+{
+    public class EventHandler
+    {
+        Config _config = JoinBroadcast.Singleton.Config;
+
+        public void OnJoining(VerifiedEventArgs ev)
+        {
+            string joinBroadcastBuilder = _config.JoinBroadcast.Replace("%PlayerName%", ev.Player.Nickname);
+
+            ev.Player.Broadcast(_config.JoinBroadCastDuration, joinBroadcastBuilder);
+        }
+    }
+}
